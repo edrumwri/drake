8442b6195382e601cd4b088dcfdb29e27c4491a7
Evan Drumwright <drum@tri.global>
2017-12-09 10:00:21 -0800

    Checkpoint.

--- systems/framework/context.h
+++ systems/framework/context.h
@@ -24,6 +24,12 @@ struct StepInfo {
   /// doubles, time resolution will gradually degrade as time increases.
   // TODO(sherm1): Consider whether this is sufficiently robust.
   T time_sec{0.0};
+
+  /// The time, in seconds, that the last discrete update was performed. Same
+  /// caveat about time resolution degradation (above) applies. The initial
+  /// conditions are treated as a discrete update, so this variable should
+  /// always hold a valid value.
+  T last_discrete_update_time_sec{0.0};
 };
 
 /// Context is an abstract base class template that represents all
@@ -52,6 +58,16 @@ class Context {
   virtual void set_time(const T& time_sec) {
     get_mutable_step_info()->time_sec = time_sec;
   }
+
+  /// Gets the time of the last discrete update, if any.
+  const T& get_last_discrete_update_time() {
+      return get_step_info().last_discrete_update_time_sec;
+  }
+
+  /// Sets the time of the last discrete update.
+  void set_last_discrete_update_time(const T& time_sec) {
+    get_mutable_step_info()->last_discrete_update_time_sec = time_sec;
+  } 
 
   // =========================================================================
   // Accessors and Mutators for State.
