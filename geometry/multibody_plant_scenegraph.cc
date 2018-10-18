d93dc14a3ca2809e0a9118cadc14c37814c6f0af
Evan Drumwright <drum@tri.global>
2018-10-18 14:11:03 -0700

    First iteration of multibody plant scenegraph.

--- geometry/multibody_plant_scenegraph.cc
+++
@@ -1,9 +0,0 @@
-#include "drake/geometry/multibody_plant_scenegraph.h"
-
-namespace drake {
-namespace geometry {
-
-template class MultibodyPlantSceneGraph<double>;
-
-}  // namespace geometry
-}  // namespace drake
